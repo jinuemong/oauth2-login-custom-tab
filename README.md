a16b1b6e6f974d2703fc3898774205880e236633
김진우 <jinwoo3661@naver.com>
2025-04-08 00:30:42 +0900

    init

---
+++ README.md
@@ -0,0 +1,11 @@
+
+## reso
+
+###  커스텀 탭 가이드
+https://developer.chrome.com/docs/android/custom-tabs/guide-get-started?hl=ko
+
+# 커스텀 탭 로그인 구현
+https://velog.io/@maratangsoft/android-apple-login#3-custom-tabs-%EC%9D%B4%EC%9A%A9
+
+# 웹뷰 리 다이렉션
+https://studyroadmap-kkm.tistory.com/174
--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.android.oauth2_login_custom_tab"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.android.oauth2_login_custom_tab"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
